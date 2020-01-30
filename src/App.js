01527a88b441e8c1a04898695a980fd05154a7e9
Megan Reyes <megangillianne@gmail.com>
2020-01-30 00:23:33 -0800

    Added TodoEntryForm

--- src/App.js
+++ src/App.js
@@ -1,18 +1,27 @@
 import React, {useState} from 'react';
 import TodoList from './TodoList';
+import TodoEntryForm from './TodoEntryForm';
 
 export default function App() {
+
   const [todos, setTodos] = useState([
      {id: 1, content: 'Play Pokemon Sword'},
      {id: 2, content: 'Fuck shit up'},
   ]);
+
   function deleteTodo(id) {
     setTodos(todos => todos.filter(todo => todo.id !== id));
   }
+
+  function addTodo(todo) {
+    setTodos(todos => [...todos, todo]);
+   }
+
   return (
      <div className="App">
       <h1>Things To Do</h1>
       <TodoList todos={todos} />
+      <TodoEntryForm adder={addTodo} />
      </div>
  );
 }
