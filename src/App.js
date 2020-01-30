5c3a09745a75b0adc5524d2b3fe7411c2cc79b55
Megan Reyes <megangillianne@gmail.com>
2020-01-30 00:14:24 -0800

    Add the delete functionlity

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import React from 'react';
+import React, {useState} from 'react';
+import TodoList from './TodoList';
 
-function App() {
+export default function App() {
+  const [todos, setTodos] = useState([
+     {id: 1, content: 'Play Pokemon Sword'},
+     {id: 2, content: 'Fuck shit up'},
+  ]);
+  function deleteTodo(id) {
+    setTodos(todos => todos.filter(todo => todo.id !== id));
+  }
   return (
-    <div className="App">
-      <header className="Things To Do">
-
-        <p>
-         hello
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Things To Do
-        </a>
-      </header>
-    </div>
-  );
+     <div className="App">
+      <h1>Things To Do</h1>
+      <TodoList todos={todos} />
+     </div>
+ );
 }
-
-export default App;
